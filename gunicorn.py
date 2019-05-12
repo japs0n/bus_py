28b09ad8b2e524fba82bcad4a078adabd579892f
japs0n <sss_14@126.com>
2019-05-12 22:09:28 +0800

    fix gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -1,12 +1,12 @@
 import multiprocessing
 
-chdir = '~/bus_py/'
+chdir = '/home/ubuntu/bus_py'
 bind = "0.0.0.0:8008"
 worker_class = 'gevent'
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 2
 reload = True
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
-accesslog = "~/bus_py/logs/gunicorn_access.log"
+accesslog = "/home/ubuntu/bus_py/logs/gunicorn_access.log"
 loglevel = 'warning'  # 错误日志等级
-errorlog = "~/bus_py/logs/gunicorn_error.log"
+errorlog = "/home/ubuntu/bus_py/logs/gunicorn_error.log"
